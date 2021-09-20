8b4732209e2de6a6369d9b95a71e4f52dc225d69
Yevgen <yevgen.yelik@gmail.com>
2021-09-20 21:33:38 +0300

    alter shorten_url logic

--- bitly_app/views.py
+++ bitly_app/views.py
@@ -1,14 +1,12 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.views.generic.base import View
+from django.shortcuts import render
+from django.views.generic.base import View, RedirectView
 
 from bitly.settings import cache
 from bitly_app.forms import LongURLForm
 import hashlib
-import pyshorteners
 
 
-shortener = pyshorteners.Shortener()
+backend = '127.0.0.1:8000/'
 
 
 class ConvertView(View):
@@ -24,16 +22,16 @@ class ConvertView(View):
         form = self.form_class(request.POST)
         if form.is_valid():
             url = form.cleaned_data['long_url']
-            hash_url = hashlib.md5(url.encode()).hexdigest()
+            hash_url = hashlib.md5(url.encode()).hexdigest()[:10]
             if not cache.hexists(hash_url, url):
-                short_url = shortener.tinyurl.short(url)
-                cache.hset(hash_url, url, short_url)
+                shorten_url = backend + hash_url
+                cache.hset(hash_url, url, shorten_url)
 
             result = cache.hget(hash_url, url)
             context = {
                 'form': self.form_class(),
                 'url': url,
-                'shorten_link': result.decode()
+                'shorten_url': result.decode()
             }
 
             return render(request, 'bitly_app/success.html', context)
@@ -41,6 +39,13 @@ class ConvertView(View):
             form = self.form_class()
         return render(request, self.template_name, {'form': form})
 
+
+class RedirectTo(RedirectView):
+    permanent = True
+    url = 'https://redis.io/commands'
+
+    def get_redirect_url(self, *args, **kwargs):
+        return super().get_redirect_url(*args, **kwargs)
 
 
 
