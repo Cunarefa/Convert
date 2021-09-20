68ccf937c56cb0477911af66208c78ae7662d289
Yevgen <yevgen.yelik@gmail.com>
2021-09-21 00:46:38 +0300

    add redirect url

--- bitly_app/urls.py
+++ bitly_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ConvertView
+from .views import ConvertView, RedirectTo
 
 urlpatterns = [
     path('long', ConvertView.as_view(), name='long'),
+    path('<str:hash_url>', RedirectTo.as_view(), name='redirect'),
 ]
